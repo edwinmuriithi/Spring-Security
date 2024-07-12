26dca0611790bafe1a5cb15445c0c47679e8d9ac
edwinmuriithi <kabuimuriithi@gmail.com>
2024-07-12 15:22:04 +0300

    feat(Entity): Edit User class

--- src/main/java/com/example/security/entity/User.java
+++ src/main/java/com/example/security/entity/User.java
@@ -1,4 +1,27 @@
 package com.example.security.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
 }
