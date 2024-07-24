be3e47a7a2ad9fcf2e42703ab96ab913cb53bdc1
edwinmuriithi <kabuimuriithi@gmail.com>
2024-07-24 11:19:57 +0300

    feat(Service): Create extractAllClaims method

--- src/main/java/com/example/security/configuration/JwtService.java
+++ src/main/java/com/example/security/configuration/JwtService.java
@@ -1,11 +1,28 @@
 package com.example.security.configuration;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "1bad21ee8a251e4f6b8953ca204404c97c6900ec65b3e4c28a5ab1488948c526";
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser().setSigningKey(getSignInKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
